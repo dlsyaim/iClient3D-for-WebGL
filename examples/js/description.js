19ebaea0eca724984db558853e26ee046a947936
Peter <bjfubjfu@gmail.com>
2016-02-02 16:50:38 +0800

    update delete and example

---
+++ examples/js/description.js
@@ -0,0 +1,7 @@
+﻿var DemoDescription = {};
+//地图
+DemoDescription['3857Map'] = {title: '墨卡托投影', desc: '定义：加载SuperMap iServer发布的影像服务', oper: '通过鼠标在地图上移动，缩放。' }
+DemoDescription['image'] = {title: 'Image图层', desc: '定义：此图层用于显示单一的图片作为地球球面影像，适合在局域网或特定情况下使用， 实现同步放大、缩小、平移等效果。', oper: '无' }
+DemoDescription['tianditu'] = {title: '天地图图层', desc: '定义：天地图WMS服务', oper: '无' }
+DemoDescription['osgb'] = {title: '倾斜摄影图层', desc: '定义：马赛倾斜摄影', oper: '无' }
+DemoDescription['terrain'] = {title: '地形图层', desc: '定义：全球地形服务', oper: '无' }
